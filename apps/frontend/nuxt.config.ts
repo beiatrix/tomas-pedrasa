3c308df1ef4029e57d0d1bc1ea0a9daaf6bff02c
beiatrix <bpedrasa@gmail.com>
2025-03-04 14:47:01 -0600

    feat: link frontend and backend

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/sanity"
+  ],
+  sanity: {
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: 'production'
+  }
 });
